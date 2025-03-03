30ee386d17c8d6d08c054cc75571d12101fae913
Alberto <alberto.renteriac@iteso.mx>
2025-03-03 08:08:53 -0600

    Add initial implementation of tensor operations in algo.py

---
+++ torch2/algo.py
@@ -0,0 +1,11 @@
+import torch
+
+random1 = torch.rand(7, 7)
+random2 = torch.rand(1, 7)
+
+result = torch.mathmul(random2, random1.T)
+
+random3 = torch.rand(1,1,1,10)
+
+random3 = random3.view(-1)
+print(random3.shape)
